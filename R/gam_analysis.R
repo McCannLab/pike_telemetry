f6b6fd717be897a59c7908cf56a96fbf342cbab1
Matt Guzzo <mattguzzo12@gmail.com>
2020-01-29 15:26:40 -0500

    add gam code

--- R/gam_analysis.R
+++ R/gam_analysis.R
@@ -1,19 +1,52 @@
 library(mgcv)
+library(tidyverse)
 
 dat <- readRDS("datf.rds")
+dat <- dat %>% filter(!is.nan(speed_mean))
+
 dat$hour <- as.integer(dat$hour)
+dat$fish_id <- as.factor(dat$fish_id)
+dat$lightson_mean <- as.factor(dat$lightson_mean)
 
 hist(sqrt(dat$depth_raw_mean))
 
-depth_mod <- gam(sqrt(depth_raw_mean) ??? s(hour, k = 24, bs = "cc") + s(fish_id, k = 10, bs = "re"),
+depth_mod <- bam(sqrt(depth_raw_mean) ~ s(hour, k = 24, bs = "cc") + season + lit_temp + lightson_mean +
+                   + s(fish_id, k = 10, bs = "re"),
                  knots = list(hour = c(0, 23)), method = "REML", family = "gaussian", data = dat)
+summary(depth_mod)
+
+plot(residuals(depth_mod))
 
 
-bird_modG <- gam(count ??? te(week, latitude, bs=c("cc", "tp"), k=c(10, 10)), 
-                 data=bird_move, method="REML", family="poisson", knots=list(week=c(0,
+hist(dat$temp_mean)
+
+temp_mod <- bam(sqrt(temp_mean) ~ s(hour, k = 24, bs = "cc") + season + lit_temp + lightson_mean +
+                   + s(fish_id, k = 10, bs = "re"),
+                 knots = list(hour = c(0, 23)), method = "REML", family = "gaussian", data = dat)
+summary(temp_mod)
+
+hist(sqrt(dat$speed_mean))
+
+activity_mod <- bam(sqrt(speed_mean) ~ s(hour, k = 24, bs = "cc") + season + lit_temp + lightson_mean +
+                  + s(fish_id, k = 10, bs = "re"),
+                knots = list(hour = c(0, 23)), method = "REML", family = "gaussian", data = dat)
+summary(activity_mod)
+
+hist(sqrt(dat$dis_2_shore_mean))
+
+distshore_mod <- bam(sqrt(dis_2_shore_mean) ~ s(hour, k = 24, bs = "cc") + season + lit_temp + lightson_mean +
+                      + s(fish_id, k = 10, bs = "re"),
+                    knots = list(hour = c(0, 23)), method = "REML", family = "gaussian", data = dat)
+summary(distshore_mod)
 
 
 
 
 
-bird_modG <- gam(count ??? te(week, latitude, bs=c("cc", "tp"), k=c(10, 10)), data=bird_move, method="REML", family="poisson", knots=list(week=c(0, 52)))
+
+
+
+
+
+bird_modG <- gam(count ~ te(week, latitude, bs=c("cc", "tp"), k=c(10, 10)), 
+                 data=bird_move, method="REML", family="poisson", knots=list(week=c(0, 52)))
