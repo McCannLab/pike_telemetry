6122729613cffadc643d4402592e1b30443192b1
Tim Bartley <timothy.bartley@gmail.com>
2020-01-29 10:56:36 -0500

    more figure updates

--- R/light_temp_plot.R
+++ R/light_temp_plot.R
@@ -1,53 +1,89 @@
+### load packages -------
+
 library(tidyverse)
 library(lubridate)
+library(ggpubr)
 
-air_temp <- read_csv("")
+
+
+### import data -------
+
+air_temp <- read_csv("data/yk_air_temp_entire_study.csv")
+
 day_len <- read_csv("data/sunrise_sunset.csv") %>%
   mutate(day_length = difftime(sunset, sunrise))
+
 water_temp <- read_csv("data/temp_string_32_interp.csv")
+
 water_light <- read_csv("data/interpolated_light_data.csv")
 
 
-day_len$date <- paste(day_len$year, day_len$month, day_len$day, sep="-") %>% ymd() %>% as.Date()
 
+### plot lake temperature profile -------
 
-
-# lake temperature profile
-ggplot(water_temp, aes(x=date, y=depth, fill=temp))+
+water_temp_plot <- ggplot(water_temp, aes(x=date, y=depth, fill=temp))+
   scale_y_reverse()+
   scale_x_date(date_break = "4 month", date_labels = "%b %Y") +
   geom_tile()+
   scale_fill_gradientn(colours = c("dark blue", "blue", "cyan", "green",  "orange", "red"))+
-  ylab("Depth (m)")+
-  xlab("Date")+
-  theme_classic(base_size=20)+
+  ylab("depth (m)")+
+  xlab("date")+
+  theme_classic(base_size=14)+
   theme(axis.text.x = element_text(colour = 'black'),
         axis.text.y = element_text(colour = 'black'))
 
-#light profile plot
-ggplot(water_light, aes(x=date, y=depth, fill=log10(light + 1)))+
+
+
+### plot light profile -------
+
+light_plot <- ggplot(water_light, aes(x=date, y=depth, fill=log10(light + 1)))+
   scale_y_reverse()+
   scale_x_date(date_break = "4 month", date_labels = "%b %Y") +
   geom_tile()+
   scale_fill_gradientn(colours = c("white", "dark blue", "blue", "cyan", "green",  "orange", "red"))+
-  ylab("Depth (m)")+
-  xlab("Date")+
-  theme_classic(base_size=20)+
-  theme(axis.text.x = element_text(colour = 'black'),
-        axis.text.y = element_text(colour = 'black'))+
-  theme(legend.justification=c(0,0), legend.position=c(0.15, 0.05))+
+  ylab("depth (m)")+
+  xlab("date")+
   labs(fill = "log10(lux)") +
+  theme_classic(base_size=14)+
+  theme(axis.text.x = element_text(colour = 'black'),
+    axis.text.y = element_text(colour = 'black'))+
+  theme(legend.justification=c(0,0), legend.position=c(0.15, 0.05))+
   guides(colour = F, shape = F)
 
-ggplot(ec, aes(x=date, y=mean_temp, colour=mean_temp))+
+
+
+### plot air temperature -------
+
+air_temp_plot <- ggplot(air_temp, aes(x=date, y=mean_temp, colour=mean_temp))+
   geom_point()+
   scale_x_date(date_break = "4 month", date_labels = "%b %Y") +
-  theme_bw(base_size=20)+
-  ylab("Daily air temperature (�C)")+
-  xlab("Date")+
+  ylab("daily air temperature (C)")+
+  xlab("date")+
+  labs(colour = "temperature") +
+  theme_classic(base_size=14)+
   theme(axis.text.x = element_text(colour = 'black'),
-        axis.text.y = element_text(colour = 'black'))+
+    axis.text.y = element_text(colour = 'black'))+
   scale_colour_gradientn(colours = rev(rainbow(5))) +
   theme(legend.justification=c(0,0), legend.position=c(0, 0.05))+
-  theme(legend.title=element_blank())+
   theme(legend.background = element_rect(colour = NA, fill = NA))
+
+
+
+### multiplot -------
+
+multiplot <- ggarrange(air_temp_plot, 
+  water_temp_plot, 
+  light_plot, 
+  air_temp_plot,
+  labels = c("A", "B", "C", "D"),
+  font.label = list(size = 18),
+  ncol = 2,
+  nrow = 2, 
+  align = "h")
+
+ggsave(filename = "figures/supplementary_figure_4.png", 
+  plot = supplementary_figure_4,
+  width = 12,
+  height = 12,
+  dpi = "print")
+
